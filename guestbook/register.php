86660d77e430a2417d5175c9ef80202bdc7ec9c4
Olegdev22 <example@gmail.com>
2026-03-12 17:18:07 +0200

    add db conection

--- guestbook/register.php
+++ guestbook/register.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once __DIR__ . "/incs/db.php";
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -73,7 +79,7 @@
         </div>
       
       </div>
-      
+    
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
