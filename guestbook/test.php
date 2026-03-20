0e948f5ad6b916478eeff75b265d77891dff3fc5
Olegdev22 <kirbaba2006@gmail.com>
2026-03-21 01:25:34 +0200

    file test -a simple example of pagination

---
+++ guestbook/test.php
@@ -0,0 +1,29 @@
+<?php
+// Принцип пагинации
+
+require_once __DIR__ . '/incs/functions.php';
+
+$data = range(1, 101);
+$per_page = 10; // сколько выводить записей
+$total = count($data);
+var_dump($total);
+
+$pages_cnt = ceil($total / $per_page);
+
+// вывод записей согласно странице
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+if ($page < 1) {
+    $page = 1;
+}
+
+if ($page > $pages_cnt) {
+    $page = $pages_cnt;
+}
+
+$start = ($page - 1) * $per_page;
+
+dump(array_slice($data, $start, $per_page));
+
+for ($i = 1; $i <= $pages_cnt; $i++) {
+    echo "<a href='?page={$i}'> {$i}</a>";
+}
